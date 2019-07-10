cec807a3a8d7ceecd75f01dcaddc51a6f218fd34
Adrien JOUBERT <adjouber@e1r12p7.42.fr>
2019-07-10 13:31:35 +0200

    adding t_gui

--- includes/unknow_project_game_engine.h
+++ includes/unknow_project_game_engine.h
@@ -1,5 +1,9 @@
 #ifndef UNKNOW_PROJECT_GAME_ENGINE_H
 # define UNKNOW_PROJECT_GAME_ENGINE_H
 
+typedef struct  s_gui
+{
+    
+}               t_gui;
 
 #endif
--- srcs/gui/t_gui.c
+++ srcs/gui/t_gui.c
@@ -1,1 +1,20 @@
 #include "unknow_project.h"
+
+t_gui create_t_gui()
+{
+	t_gui result;
+
+	return (result);
+}
+
+t_gui *initialize_t_gui()
+{
+	t_gui *result;
+
+	if (!(result = (t_gui *)malloc(sizeof(t_gui))))
+		error_exit(-13, "Can't create a t_gui");
+
+	*result = create_t_gui();
+
+	return (result);
+}
