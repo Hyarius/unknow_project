176134fce51c748550e00bf64db6335e50ba2d5e
Gregory BOUTIN <gboutin@e1r11p6.42.fr>
2019-08-21 10:15:52 +0200

    norme completted on draw_t_mesh.c

--- srcs/mesh/draw_t_mesh.c
+++ srcs/mesh/draw_t_mesh.c
@@ -1,13 +1,13 @@
 #include "unknow_project.h"
 
-static void		init_points_uv(t_mesh *mesh, t_face face, t_vector3 *points_uv)
+void		init_points_uv(t_mesh *mesh, t_face face, t_vector3 *points_uv)
 {
 	points_uv[0] = t_vector3_list_at(mesh->uvs, face.index_uvs[0]);
 	points_uv[1] = t_vector3_list_at(mesh->uvs, face.index_uvs[1]);
 	points_uv[2] = t_vector3_list_at(mesh->uvs, face.index_uvs[2]);
 }
 
-static void		init_points(t_mesh *mesh, t_face face, t_vector3 *points)
+void		init_points(t_mesh *mesh, t_face face, t_vector3 *points)
 {
 	points[0] = add_vector3_to_vector3(t_vector3_list_at(mesh->vertices,\
 									face.index_vertices[0]), mesh->pos);
@@ -17,8 +17,7 @@ static void		init_points(t_mesh *mesh, t_face face, t_vector3 *points)
 									face.index_vertices[2]), mesh->pos);
 }
 
-
-static void	fonction_boucle_2(t_mesh *mesh, int j, t_camera *p_cam, t_face face, t_color darkness_color)
+t_triangle	init_triangle_clipped(t_mesh *mesh, t_camera *p_cam, int j)
 {
 	t_triangle	triangle;
 
@@ -28,33 +27,37 @@ static void	fonction_boucle_2(t_mesh *mesh, int j, t_camera *p_cam, t_face face,
 											&(p_cam->projection));
 	triangle.c = apply_t_camera(&(p_cam->clipping_list[j + 2]),\
 											&(p_cam->projection));
-	if (mesh->texture == NULL)
+	if (mesh->texture != NULL)
 	{
-		t_color_list_push_back(&(p_cam->color_list),\
-						fuze_t_color(face.color, darkness_color));
-		t_triangle_list_push_back(&(p_cam->triangle_color_list),\
-														triangle);
-	}
-	else
-	{
-		t_triangle_list_push_back(&(p_cam->triangle_texture_list),\
-														triangle);
+		t_triangle_list_push_back(&(p_cam->triangle_texture_list), triangle);
 		triangle.a = p_cam->clipping_list_uv[j + 0];
 		triangle.b = p_cam->clipping_list_uv[j + 1];
 		triangle.c = p_cam->clipping_list_uv[j + 2];
-		t_uv_list_push_back(&(p_cam->uv_list),\
-							create_t_uv(triangle, mesh->texture));
-		t_color_list_push_back(&(p_cam->darkness_list),\
-													darkness_color);
 	}
+	return (triangle);
 }
 
-static void	fonction_if(t_mesh *mesh, t_face face, t_camera *p_cam, t_vector3 *points, t_vector3 *points_uv)
+void		apply_darkness_on_mesh_with_texture(t_mesh *mesh, t_camera *p_cam, t_color darkness_color, t_triangle triangle)
 {
-	t_color	darkness_color;
-	float	darkness;
-	int		nb_clipped;
-	int		j;
+	t_uv_list_push_back(&(p_cam->uv_list),\
+						create_t_uv(triangle, mesh->texture));
+	t_color_list_push_back(&(p_cam->darkness_list), darkness_color);
+}
+
+void		apply_darkness_on_mesh_without_texture(t_camera *p_cam, t_face face, t_color darkness_color, t_triangle triangle)
+{
+	t_color_list_push_back(&(p_cam->color_list),\
+					fuze_t_color(face.color, darkness_color));
+	t_triangle_list_push_back(&(p_cam->triangle_color_list),\
+													triangle);
+}
+
+void		find_darkness(t_mesh *mesh, t_face face, t_camera *p_cam, int nb_clip)
+{
+	t_triangle	triangle;
+	t_color		darkness_color;
+	float		darkness;
+	int			j;
 
 	darkness = (-dot_t_vector3(face.normale, p_cam->sun_direction) + 2)\
 													/ (2 / (1 - 0.1));
@@ -66,25 +69,23 @@ static void	fonction_if(t_mesh *mesh, t_face face, t_camera *p_cam, t_vector3 *p
 	darkness_color.g = darkness;
 	darkness_color.b = darkness;
 	darkness_color.a = 0.4f;
-	points[0] = mult_vector3_by_matrix(points[0], (p_cam->view));
-	points[1] = mult_vector3_by_matrix(points[1], (p_cam->view));
-	points[2] = mult_vector3_by_matrix(points[2], (p_cam->view));
-	if (mesh->texture != NULL)
-		nb_clipped = clip_triangle_to_plane(p_cam, points, points_uv);
-	else
-		nb_clipped = clip_triangle_to_plane(p_cam, points, NULL);
 	j = 0;
-	while (j < nb_clipped)
+	while (j < nb_clip)
 	{
-		fonction_boucle_2(mesh, j, p_cam, face, darkness_color);
+		triangle = init_triangle_clipped(mesh, p_cam, j);
+		if (mesh->texture == NULL)
+			apply_darkness_on_mesh_without_texture(p_cam, face, darkness_color, triangle);
+		else
+			apply_darkness_on_mesh_with_texture(mesh, p_cam, darkness_color, triangle);
 		j += 3;
 	}
 }
 
-static void	fonction_boucle_1(t_mesh *mesh, t_face face, t_camera *p_cam)
+void		how_many_points_clipped(t_mesh *mesh, t_face face, t_camera *p_cam)
 {
 	t_vector3	points[3];
 	t_vector3	points_uv[3];
+	int			nb_clipped;
 	float		result;
 
 	init_points(mesh, face, &*points);
@@ -93,7 +94,16 @@ static void	fonction_boucle_1(t_mesh *mesh, t_face face, t_camera *p_cam)
 	result = dot_t_vector3(face.normale, normalize_t_vector3(\
 					substract_vector3_to_vector3(points[0], p_cam->pos)));
 	if (result < 0)
-		fonction_if(mesh, face, p_cam, &*points, &*points_uv);
+	{
+		points[0] = mult_vector3_by_matrix(points[0], (p_cam->view));
+		points[1] = mult_vector3_by_matrix(points[1], (p_cam->view));
+		points[2] = mult_vector3_by_matrix(points[2], (p_cam->view));
+		if (mesh->texture != NULL)
+			nb_clipped = clip_triangle_to_plane(p_cam, points, points_uv);
+		else
+			nb_clipped = clip_triangle_to_plane(p_cam, points, NULL);
+		find_darkness(mesh, face, p_cam, nb_clipped);
+	}
 }
 
 void		draw_t_mesh(t_camera *p_cam, t_mesh *mesh)
@@ -107,6 +117,6 @@ void		draw_t_mesh(t_camera *p_cam, t_mesh *mesh)
 	while (++i < mesh->faces->size)
 	{
 		face = t_face_list_at(mesh->faces, i);
-		fonction_boucle_1(mesh, face, p_cam);
+		how_many_points_clipped(mesh, face, p_cam);
 	}
 }
