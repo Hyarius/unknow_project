88575a20678ea950db9661532accbc3f8998f051
Gregory BOUTIN <gboutin@e2r13p10.42.fr>
2020-01-29 14:25:20 +0100

    clean branch

--- includes/unknow_project_algorythm.h
+++
@@ -1,9 +0,0 @@
-#ifndef UNKNOW_PROJECT_ALGORYTHM_H
-# define UNKNOW_PROJECT_ALGORYTHM_H
-
-# include "unknow_project_mesh.h"
-
-int         is_t_mesh_intersecting(t_mesh *mesh_compared, t_mesh *mesh_target);
-t_mesh_list *read_map_file(int fd);
-t_mesh      *cast_ray(t_engine *engine, t_vector4 pos, t_vector4 direction, char *shooter);
-#endif
